8c8618681856fbb3feb46f84fccb5bfb2d409d2d
BrickSortingMachine <BrickSortingMachine@gmail.com>
2025-08-05 15:50:03 +0200

    First try done

--- sorter/asrs_service/asrs_service.py
+++ sorter/asrs_service/asrs_service.py
@@ -1,6 +1,14 @@
 import logging
 
 import sorter.network.tcp_client
+from grbl_streamer import GrblStreamer
+
+
+def grbl_callback(eventstring, *data):
+    args = []
+    for d in data:
+        args.append(str(d))
+    logging.info("GRBL CALLBACK: event={}".format(eventstring.ljust(30), ", ".join(args)))
 
 
 class ASRSTcpClient(sorter.network.tcp_client.TcpClient):
@@ -12,10 +20,10 @@ class ASRSTcpClient(sorter.network.tcp_client.TcpClient):
         type,
         retry_connection,
         auto_reconnect,
-        notificaiton_service,
+        asrs_service,
     ):
         super().__init__(host, port, name, type, retry_connection, auto_reconnect)
-        self.notificaiton_service: NotificationService = notificaiton_service
+        self.asrs_service: ASRSService = asrs_service
 
     def event_msg_received(self, msg):
         part_list = str(msg, "utf-8").split(" ")
@@ -23,10 +31,10 @@ class ASRSTcpClient(sorter.network.tcp_client.TcpClient):
         try:
             # notification request
             if part_list[0] == "NTF":
-                logging.info(f"Received notificaiton request - msg: {msg}")
+                logging.info(f"Received notification request - msg: {msg}")
                 notification_type = part_list[1]
                 notification_msg = " ".join(part_list[2:])
-                self.notificaiton_service.notify(notification_type, notification_msg)
+                self.asrs_service.notify(notification_type, notification_msg)
         except Exception:
             logging.error(
                 "Decoding network message error - could be malformed/entangled messages"
@@ -35,8 +43,9 @@ class ASRSTcpClient(sorter.network.tcp_client.TcpClient):
 
 class ASRSService:
     def __init__(
-        self, host: str, disable_network, 
+        self, host: str, disable_network,
     ) -> None:
+
         # network thread
         if not disable_network:
             self.tcp_client = ASRSTcpClient(
@@ -46,15 +55,27 @@ class ASRSService:
                 "ASRSService",
                 retry_connection=True,
                 auto_reconnect=True,
-                notificaiton_service=self,
+                asrs_service=self,
             )
             self.tcp_client.start()
         else:
             self.tcp_client = None
 
+        # star grbl stream
+        self.device_path = "/dev/serial/by-path/pci-0000:00:14.0-usb-0:2:1.0-port0"
+        self.grbl = GrblStreamer(grbl_callback)
+        self.grbl.setup_logging()
+        self.grbl.cnect(self.device_path, 115200)
+        self.grbl.poll_start()
+
     def stop(self):
+        self.grbl.disconnect()
         if self.tcp_client is not None:
             self.tcp_client.stop()
 
     def notify(self, notification_type, notification_msg):
         pass
+
+    def stream_gcode(self):
+        gcode = "G0 X10 Y10\nG0 X0 Y0\n"
+        self.grbl.stream(gcode)

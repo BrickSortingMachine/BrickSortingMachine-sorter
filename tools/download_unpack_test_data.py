618ce1db09525e370572bb44b8172f81332c3d40
BrickSortingMachine <BrickSortingMachine@gmail.com>
2025-08-30 23:35:13 +0200

    Added more recordings and classification model

--- tools/download_unpack_test_data.py
+++ tools/download_unpack_test_data.py
@@ -13,8 +13,10 @@ import sorter.util.argument_parser
 import sorter.util.file_hash
 
 file_list = [
-    # currently deactivated until new test recordings are uploaded
-    "rec_2023-08-09_21-38-43_0f5ca2e91a5d6718f4a17a0d4b1346553439c350.zip",
+    ("data", "rec_2023-08-09_21-38-43_0f5ca2e91a5d6718f4a17a0d4b1346553439c350.zip"),
+    ("data", "rec_2025-08-30_23-10-23_e021c0eb7f8b4eefc3706b5531a891d8e842acdb.zip"),
+    ("data", "rec_2025-08-30_23-10-59_d8cd2f93dceffcb851454d31e8617d897eff5a07.zip"),
+    ("models", "model_2024-08-04_12-44-43_multi-view_abandoned-space_583605d36ee25c8afd30c391100ccfb526aa00ed.zip"),
 ]
 
 if __name__ == "__main__":
@@ -36,28 +38,22 @@ if __name__ == "__main__":
     parser = sorter.util.argument_parser.ArgumentParser(
         description="Download Test Data"
     )
-    parser.add_argument("--name", required=False)
     args = parser.parse_args()
-
-    # data folder
-    data_path = pathlib.Path(__file__).parents[1].resolve() / "data"
-    if not data_path.exists():
-        raise Exception(f"Data folder {str(data_path)} does not exist.")
-
-    # explicit name
-    if args.name is not None:
-        file_list = [args.name]
-
-    logging.info("Will download the following files:")
-    for n in file_list:
-        logging.info(f"  {n}")
 
     count_downloaded = 0
     count_exist = 0
     logging.info(" ")
     logging.info("Starting file download ...")
     logging.info(" ")
-    for fn in file_list:
+    for info in file_list:
+        tgt_folder = info[0]
+        fn = info[1]
+        
+        # data folder
+        data_path = pathlib.Path(__file__).parents[1].resolve() / tgt_folder
+        if not data_path.exists():
+            raise Exception(f"Data folder {str(data_path)} does not exist.")
+
         file_path = data_path / fn
         logging.info(f"Package: {str(fn)}")
         if not file_path.exists():

0b664fe291217701368d162323f2127d45223613
BrickSortingMachine <BrickSortingMachine@gmail.com>
2025-08-30 23:37:34 +0200

    Fix black for download

--- tools/download_unpack_test_data.py
+++ tools/download_unpack_test_data.py
@@ -16,7 +16,10 @@ file_list = [
     ("data", "rec_2023-08-09_21-38-43_0f5ca2e91a5d6718f4a17a0d4b1346553439c350.zip"),
     ("data", "rec_2025-08-30_23-10-23_e021c0eb7f8b4eefc3706b5531a891d8e842acdb.zip"),
     ("data", "rec_2025-08-30_23-10-59_d8cd2f93dceffcb851454d31e8617d897eff5a07.zip"),
-    ("models", "model_2024-08-04_12-44-43_multi-view_abandoned-space_583605d36ee25c8afd30c391100ccfb526aa00ed.zip"),
+    (
+        "models",
+        "model_2024-08-04_12-44-43_multi-view_abandoned-space_583605d36ee25c8afd30c391100ccfb526aa00ed.zip",
+    ),
 ]
 
 if __name__ == "__main__":
@@ -48,7 +51,7 @@ if __name__ == "__main__":
     for info in file_list:
         tgt_folder = info[0]
         fn = info[1]
-        
+
         # data folder
         data_path = pathlib.Path(__file__).parents[1].resolve() / tgt_folder
         if not data_path.exists():

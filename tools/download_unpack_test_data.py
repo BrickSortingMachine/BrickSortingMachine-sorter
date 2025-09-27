47f944931a8c92d5cb147aaeea3c3814c5a41a0e
BrickSortingMachine <BrickSortingMachine@gmail.com>
2025-09-27 14:51:10 +0200

    Fix download script

--- tools/download_unpack_test_data.py
+++ tools/download_unpack_test_data.py
@@ -79,7 +79,6 @@ if __name__ == "__main__":
         actual_hash = sorter.util.file_hash.file_hash(str(file_path))
         if specified_hash == actual_hash:
             logging.info("Hash verified to be correct 🆗")
-            pass
         else:
             logging.error(" ")
             logging.error(" ")
